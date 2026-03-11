0699451aff75bbcb0232853997bee796806d6346
clement <clement.duboisdbs@gmail.com>
2026-03-11 16:35:17 +0100

    Prepare pipeline

---
+++ src/pipeline.py
@@ -0,0 +1,16 @@
+from models import Models
+# from clean_data
+# from vectorized_data
+
+def main():
+    # 1. Vectoriser train/test    
+    # 2. Scaler si nécessaire
+    # 3. GridSearchCV avec cv=5 (stratified)
+    # 4. Entraîner sur train
+    # 5. Évaluer sur test
+    # 6. Sauvegarder modèle
+    pass
+
+
+if __name__ == "__main__":
+    main()
